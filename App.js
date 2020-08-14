a48a3e727b4673e79f80a5976b4589cb4b800d38
bmalkmus <bmalkmus23@gmail.com>
2020-08-13 23:32:12 -0700

    added buttons to table

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import Test from './components/test';
 import TestTable from './components/Table';
-
+import employees from './employees.json';
 
 function App() {
+  const [employeeList, setEmployees] = useState(employees);
   return (
     <View>
       <Text>Where is my text?</Text>
-      <Text>Simple Test</Text>
+      <Text>Simple Test a really long update test</Text>
       <Test />
-      <TestTable />
+      <TestTable employees={employeeList} setEmployees={setEmployees} />
     </View>
   );
 }
--- components/Table/index.js
+++ components/Table/index.js
@@ -1,26 +1,68 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {View} from 'react-native';
-import {Table, Row, Rows} from 'react-native-table-component';
-import employees from '../../employees.json';
+/* eslint-disable react-native/no-inline-styles */
+import React , {useState} from 'react';
+import {View, TouchableOpacity, Alert, Text, StyleSheet} from 'react-native';
+import {Table, Row, TableWrapper, Cell} from 'react-native-table-component';
+// import employees from '../../employees.json';
 
-function TestTable() {
-    const tableHead = ['ID', 'Alias', 'Name', 'Strength'];
-    const tableData = employees;
-    let Data = []
+function TestTable({employees}) {
+    const tableHead = ['ID', 'Alias', 'Name', 'Strength', 'Activate'];
+
+    let Data = [];
+
     for (let i = 0; i < employees.length; i++){
-      let temp = [employees[i].id, employees[i].hero, employees[i].name, employees[i].strength];
-      Data.push(temp);
+      if (!employees[i].active){
+        let temp = [employees[i].id, employees[i].hero, employees[i].name, employees[i].strength, employees[i].active];
+        Data.push(temp);
+      }
     }
 
+     function alert(index) {
+      const alias = employees[index].hero;
+      Alert.alert(`${alias} , Agent ID ${index + 1} , has been activated on mission`);
+      setCount(count + 1);
+    }
+
+    const element = (data, index) => (
+      <TouchableOpacity onPress={alert(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Activate</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    const [count, setCount] = useState(0);
+
   return (
-    <View>
-      <Table>
-        <Row data={tableHead} />
-        <Rows data={Data}/>
-      </Table>
-    </View>
+    <View style={styles.container}>
+      <Text>
+        Count: {count}
+      </Text>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+          {
+            Data.map((rowData, index) => (
+              <TableWrapper key={index} style={styles.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 4 ? element(cellData, index) : cellData} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff', height:800 },
+  head: { height: 40, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1', height: 50 },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' },
+});
 
 export default TestTable;
